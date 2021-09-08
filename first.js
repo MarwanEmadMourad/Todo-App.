22928dcadd19d0021d9d056b79eac20c80716b54
MarwanEmadMourad <marwanemad36@gmail.com>
2021-09-08 22:25:26 +0200

    Adding input field with a button.

--- first.js
+++ first.js
@@ -1,33 +1,11 @@
-const todo = [{
-    text:'Do Your Prayers',
-    completed:0
-},{
-    text:'Solve On leetcode',
-    completed:0
-},{
-    text:'Eat Breakfast',
-    completed:1
-},{
-    text:'Visit the Doctor',
-    completed:0
-},{
-    text:'Finish your project',
-    completed:1
-}]
+const todos = []
 
-
-document.querySelector('#show').addEventListener('click',function(e){
-    const after_that = document.querySelector('#up') ;
-    for (let i = 0 ; i<todo.length ;i++){
-        let child = document.createElement('p')
-        child.textContent = todo[i].text ;
-        after_that.appendChild(child)
-    }
+document.querySelector('#input-todo').addEventListener('change',function(e){
+    const text = e.target.value 
+    todos.push(text)
+    console.log(todos[todos.length-1])
+})
+document.querySelector('#add-todo').addEventListener('click',function(){
+    todos.push(text)
 })
 
-document.querySelector('#Dont-show').addEventListener('click',function(){
-    const all_todos = document.querySelectorAll('p')
-    all_todos.forEach(todo => {
-        todo.remove()
-    });
-})
