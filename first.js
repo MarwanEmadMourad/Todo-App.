ba1ea6b198cc99ede304baad7475360cf29296e9
MarwanEmadMourad <marwanemad36@gmail.com>
2021-09-09 00:16:28 +0200

    Render the added todos and removing them.

--- first.js
+++ first.js
@@ -1,11 +1,30 @@
 const todos = []
+let text = ''
 
-document.querySelector('#input-todo').addEventListener('change',function(e){
-    const text = e.target.value 
-    todos.push(text)
-    console.log(todos[todos.length-1])
+// taking the text input from the input field
+document.querySelector('#input-todo').addEventListener('input',function(e){
+    text = e.target.value 
 })
+
+// appending the input text to our todos list
 document.querySelector('#add-todo').addEventListener('click',function(){
+    document.querySelector('#input-todo').value = ''
     todos.push(text)
+    // console.log(todos[todos.length-1])
+    render_todos(todos)
 })
+
+// removing all our todos from our screen and from our list
+document.querySelector('#remove-todos').addEventListener('click',function(){
+    document.querySelector('#all-content').innerHTML = ''
+    while(todos.length)
+        todos.pop()
+})
+
+// rendering our todos in our div
+const render_todos = function(todos){
+        const added = document.createElement('p')
+        added.textContent = todos[todos.length-1]
+        document.querySelector('#all-content').appendChild(added)
+}
 
